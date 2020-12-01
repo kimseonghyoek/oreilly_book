6845c6f757ce299fd8eeaf04cea90401c58d92ba
Overloper <51446128+kimseonghyoek@users.noreply.github.com>
2020-12-01 23:16:57 +0900

    Array.filter 메서드

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const schools = [
   "Wakefield"
 ]
 
-console.log( schools.join(", ") )
+const wSchools = schools.filter(school => school[0] === "W");
+
+console.log( wSchools )
 
 
 ReactDOM.render(
