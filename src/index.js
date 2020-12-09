2336c8f9d5cdac36fa0b2757cc1aaa94b875971a
Overloper <51446128+kimseonghyoek@users.noreply.github.com>
2020-12-09 23:54:19 +0900

    고차함수

--- src/index.js
+++ src/index.js
@@ -4,38 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const colors = [
-  {
-    id: '-xekare',
-    title: "과격한 빨강",
-    rating: 3
-  },
-  {
-    id: '-jbwsof',
-    title: "큰 파랑",
-    rating: 2
-  },
-  {
-    id: '-prigbj',
-    title: "회색곰 회색",
-    rating: 5
-  },
-  {
-    id: '-ryhbhsl',
-    title: "바나나",
-    rating: 1
-  }
-]
+const invokeIf = (condition, fnTure, fnFalse) =>
+  (condition) ? fnTure() : fnFalse()
 
-const hashColors = colors.reduce(
-  (hash, {id, title, rating}) => {
-    hash[id] = {title, rating}
-    return hash
-  },
-  {}
-)
+const showWelcome = () =>
+  console.log("Welcome!!!")
 
-console.log(hashColors)
+const showUnauthorized = () =>
+  console.log("Unauthorized!!!")
+
+invokeIf(true, showWelcome, showUnauthorized)
+invokeIf(false, showWelcome, showUnauthorized)
 
 ReactDOM.render(
   <React.StrictMode>
