7f93f6906c1a7efa1f9b957e04770e405350ff7d
Overloper <51446128+kimseonghyoek@users.noreply.github.com>
2020-12-14 22:40:25 +0900

    재귀3

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const countdown = (value, fn, delay=1000) => {
-  fn(value)
-  return (value > 0) ?
-    setTimeout(() => countdown(value-1, fn), delay) :
-    value
+const deepPick = (fields, object={}) => {
+  const [first, ...remaining] = fields.split(".")
+  return (remaining.length) ?
+    deepPick(remaining.join("."), object[first]) :
+    object[first]
 }
 
-const log = value => console.log(value)
-countdown(10, log)
+const dan = {
+  type: "person",
+  data: {
+    gender: "male",
+    info: {
+      id: 22,
+      fullname: {
+        first: "Dan",
+        last: "Deacon"
+      }
+    }
+  }
+}
+
+console.log( deepPick("type", dan))
+console.log( deepPick("data.info.fullname.first", dan))
 
 ReactDOM.render(
   <React.StrictMode>
